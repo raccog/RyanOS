10b8710f5e98f519950cf4c9b8194c0f690deefc
Ryan Cohen <rcohenprogramming@gmail.com>
2022-04-14 17:54:28 -0400

    Add check to ensure Output is set.

--- libc/stdio.c
+++ libc/stdio.c
@@ -1,6 +1,8 @@
 #include "stdio.h"
 
-static Output output;
+static Output output = {
+    .write_string = NULL
+};
 
 void set_output(WriteString write_string) {
     output.write_string = write_string;
@@ -8,12 +10,18 @@ void set_output(WriteString write_string) {
 
 int putc(char c) {
     char str[2];
+    if (output.write_string == NULL) {
+        return -1;
+    }
     str[0] = c;
     str[1] = '\0';
     return output.write_string((const char *)str);
 }
 
 int puts(const char *str) {
+    if (output.write_string == NULL) {
+        return -1;
+    }
     return output.write_string(str);
 }
 
